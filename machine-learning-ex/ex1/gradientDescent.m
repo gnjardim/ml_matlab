b6114e7f6b9d7d107279dc07dc80f0d5e337e58c
gnjardim <48603966+gnjardim@users.noreply.github.com>
2020-03-15 16:00:52 -0300

    finish 1st exercise

--- machine-learning-ex/ex1/gradientDescent.m
+++ machine-learning-ex/ex1/gradientDescent.m
@@ -17,8 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
+    theta = theta - (alpha*(1/m)*sum((X*theta - y)'*X, 1))';
+    %temp1 = theta(1) - alpha*(1/m)*sum((X*theta - y)'*X(:, 1));
+    %temp2 = theta(2) - alpha*(1/m)*sum((X*theta - y)'*X(:, 2));
 
 
 
